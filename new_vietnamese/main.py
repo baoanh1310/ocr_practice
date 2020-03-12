4913c8df3b8295e5645db4ffc92b677c6633758a
baoanh1310 <dataholicguy@gmail.com>
2020-03-12 21:58:54 +0700

    Test OCR on input image for converting to string

--- new_vietnamese/main.py
+++ new_vietnamese/main.py
@@ -1,8 +1,27 @@
+# USAGE
+# python main.py --image test.png
+
+import argparse
+import time
+
 from converter import *
+from ocr import *
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+    help="Path to the image for OCR")
+args = vars(ap.parse_args())
 
 if __name__ == "__main__":
 
-    str = "Phạm Ngọc Bảo Anh"
+    imgPath = args["image"]
+    model = OCR()
 
-    result = Converter(str=str)
-    print(result.convert())
+    start = time.time()
+
+    str = model.ocr_from_image(imgPath=imgPath)
+
+    end = time.time()
+
+    print(str)
+    print('Time: {}'.format(end-start))
--- new_vietnamese/ocr.py
+++ new_vietnamese/ocr.py
@@ -1,6 +1,8 @@
 import pytesseract
 from PIL import Image
 
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+
 class OCR(object):
     def ocr_from_image(self, imgPath, lang = 'vie'):
         return pytesseract.image_to_string(Image.open(imgPath), lang=lang)
