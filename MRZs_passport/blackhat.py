6cb90d13d232923da15c43fbf8f6f9bc59275a1f
baoanh1310 <dataholicguy@gmail.com>
2020-03-24 00:15:52 +0700

    Black Hat Morphological Transformation

---
+++ MRZs_passport/blackhat.py
@@ -0,0 +1,21 @@
+# Example of using Blackhat Morphological Transformation using OpenCV
+# USAGE
+# python blackhat.py --image j.png
+import cv2
+import argparse
+import numpy as np 
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, 
+	help="Path to the image")
+args = vars(ap.parse_args())
+
+# read image, blackhat, save output
+img = cv2.imread(args["image"], 0)
+kernel = np.ones((5, 5), np.uint8)
+
+# Black Hat is the difference between the closing of the input image and input image
+blackhat = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
+
+cv2.imwrite("j_blackhat.png", blackhat)
