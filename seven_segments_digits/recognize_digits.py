dd3a750f82e9d6ee6fa315418678f8c34bc110f0
baoanh1310 <dataholicguy@gmail.com>
2020-03-25 00:03:59 +0700

    Add DEBUG mode and Add result of step 3: Extract digits from the LCD

--- seven_segments_digits/recognize_digits.py
+++ seven_segments_digits/recognize_digits.py
@@ -18,6 +18,9 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(blurred, 50, 200, 255)
 # cv2.imwrite("step1_result.png", edged)
+if DEBUG == True:
+	cv2.imshow("Step 1 result", edged)
+	cv2.waitKey(0)
 
 # Step 2: Extract the LCD
 # find contours in the edge map, then sort them by their area
@@ -43,4 +46,41 @@ for c in cnts:
 # extract the thermostat display, apply a perspective transform to it
 warped = four_point_transform(gray, displayCnt.reshape(4, 2))
 output = four_point_transform(image, displayCnt.reshape(4, 2))
-cv2.imwrite("step2_result.png", output)
+# cv2.imwrite("step2_result.png", output)
+if DEBUG == True:
+	cv2.imshow("Step 2 Result", output)
+	cv2.waitKey(0)
+
+# Step 3: Extract the Digits from the LCD
+# threshold the warped image, then apply a series of morphological
+# operations to cleanup the thresholded image
+thresh = cv2.threshold(warped, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+
+if DEBUG == True:
+	cv2.imshow("Morphological result", thresh)
+	cv2.waitKey(0)
+
+# find contours in the thresholded image, then initialize the digit contours lists
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+digitCnts = []
+
+# loop over the digit area candidates
+for c in cnts:
+	# compute the bounding box of the contour
+	(x, y, w, h) = cv2.boundingRect(c)
+
+	# if the contour is sufficiently large, it must be a digit
+	if w >= 15 and (h >= 30 and h <= 40):
+		digitCnts.append(c)
+
+		new_output = cv2.rectangle(output, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+cv2.imwrite("step3_result.png", new_output)
+if DEBUG == True:
+	cv2.imshow("Step 3 result", new_output)
+	cv2.waitKey(0)
