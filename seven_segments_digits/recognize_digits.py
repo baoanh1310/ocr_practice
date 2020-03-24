391469dd690a123982b71b5dd453a6ac03e6fd07
baoanh1310 <dataholicguy@gmail.com>
2020-03-25 00:14:43 +0700

    Finish recognizer and Add result of step 4: Identify 7-segments digits

--- seven_segments_digits/recognize_digits.py
+++ seven_segments_digits/recognize_digits.py
@@ -80,7 +80,64 @@ for c in cnts:
 
 		new_output = cv2.rectangle(output, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-cv2.imwrite("step3_result.png", new_output)
+# cv2.imwrite("step3_result.png", new_output)
 if DEBUG == True:
 	cv2.imshow("Step 3 result", new_output)
 	cv2.waitKey(0)
+
+# Step 4: identify each of the digits
+# sort the contours from left-to-right based on their (x, y)-coordinates
+# then initialize the actual digits themselves
+digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
+digits = []
+
+# loop over each of the digits
+for c in digitCnts:
+	# extract the digit ROI
+	(x, y, w, h) = cv2.boundingRect(c)
+	roi = thresh[y:y+h, x:x+w]
+
+	# compute the width and height of each of the 7-segments
+	# we are going to examine
+	(roiH, roiW) = roi.shape
+	(dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+	dHC = int(roiH * 0.05)
+
+	# define the set of 7 segments
+	segments = [
+		((0, 0), (w, dH)),	# top
+		((0, 0), (dW, h // 2)),	# top-left
+		((w - dW, 0), (w, h // 2)),	# top-right
+		((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
+		((0, h // 2), (dW, h)),	# bottom-left
+		((w - dW, h // 2), (w, h)),	# bottom-right
+		((0, h - dH), (w, h))	# bottom
+	]
+	on = [0] * len(segments)
+
+	# loop over the segments
+	for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
+		# extract the segment ROI, count the total number of
+		# thresholded pixels in the segment, and then compute
+		# the area of the segment
+		segROI = roi[yA:yB, xA:xB]
+		total = cv2.countNonZero(segROI)
+		area = (xB - xA) * (yB - yA)
+		# if the total number of non-zero pixels is greater than
+		# 50% of the area, mark the segment as "on"
+		if total / float(area) > 0.5:
+			on[i]= 1
+	# lookup the digit and draw it on the image
+	digit = DIGITS_LOOKUP[tuple(on)]
+	digits.append(digit)
+	cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
+	cv2.putText(output, str(digit), (x - 10, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+
+# cv2.imwrite("step4_result.png", output)
+
+# display the digits
+print(u"{}{}.{} \u00b0C".format(*digits))
+cv2.imshow("Input", image)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
