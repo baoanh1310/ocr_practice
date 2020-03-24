7e5d3bdc72cedc26616028d294b46d006cecf5c5
baoanh1310 <dataholicguy@gmail.com>
2020-03-24 23:41:25 +0700

    Add result of step 2: Extract the LCD

--- seven_segments_digits/recognize_digits.py
+++ seven_segments_digits/recognize_digits.py
@@ -13,7 +13,34 @@ image = cv2.imread("test.jpg")
 # and computing an edge map
 image = imutils.resize(image, height=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Step 1: Localize the LCD
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(blurred, 50, 200, 255)
+# cv2.imwrite("step1_result.png", edged)
 
-cv2.imwrite("step1_result.png", edged)
+# Step 2: Extract the LCD
+# find contours in the edge map, then sort them by their area
+# in descending order
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+displayCnt = None
+
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+	# if the contour has 4 vertices, then we have found
+	# the thermostat display
+	if len(approx) == 4:
+		displayCnt = approx
+		break
+
+# extract the thermostat display, apply a perspective transform to it
+warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+output = four_point_transform(image, displayCnt.reshape(4, 2))
+cv2.imwrite("step2_result.png", output)
