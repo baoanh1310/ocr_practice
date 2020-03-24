f36944abcdbcd3812568364672286d40a194e688
baoanh1310 <dataholicguy@gmail.com>
2020-03-24 23:24:12 +0700

    Define a dictionary for looking up seven-segment digits later

---
+++ seven_segments_digits/config.py
@@ -0,0 +1,21 @@
+# define the dictionary of digit segments so we can identify
+# each digit on the thermostat
+
+# Dictionary explain:
+# The key to the table is the seven-segment array.
+# A "1" in the array indicats that the given segment is "ON"
+# and a "0" indicates that the segment is "OFF".
+# The value is the actual numerical digit itself: 0-9
+
+DIGITS_LOOKUP = {
+	(1, 1, 1, 0, 1, 1, 1): 0,
+	(0, 0, 1, 0, 0, 1, 0): 1,
+	(1, 0, 1, 1, 1, 1, 0): 2,
+	(1, 0, 1, 1, 0, 1, 1): 3,
+	(0, 1, 1, 1, 0, 1, 0): 4,
+	(1, 1, 0, 1, 0, 1, 1): 5,
+	(1, 1, 0, 1, 1, 1, 1): 6,
+	(1, 0, 1, 0, 0, 1, 0): 7,
+	(1, 1, 1, 1, 1, 1, 1): 8,
+	(1, 1, 1, 1, 0, 1, 1): 9
+}
