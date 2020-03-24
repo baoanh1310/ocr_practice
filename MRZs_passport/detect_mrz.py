8972124af96532783e6400371e39fba6c1ffafeb
baoanh1310 <dataholicguy@gmail.com>
2020-03-24 10:46:49 +0700

    Calculate gradient magnitude and apply otsu thresholding method on sample passport

---
+++ MRZs_passport/detect_mrz.py
@@ -0,0 +1,47 @@
+# USAGE
+# python detect_mrz.py ./images/
+
+from imutils import paths 
+import numpy as np 
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--images", required=True, 
+	help="Path to the images directory")
+args = vars(ap.parse_args())
+
+
+# initialize a rectangular and square structuring kernel
+# for applying morphological operations, specially the closing operation
+rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 5))
+sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 21))
+
+# loop over the input image paths
+for imagePath in paths.list_images(args["images"]):
+	# load the image, resize it, and convert it to grayscale
+	image = cv2.imread(imagePath)
+	image = imutils.resize(image, height=600)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# smooth the image using a 3x3 Gaussian Filter, then apply the blackhat 
+	# morphological operator to find dark regions on a light background
+	gray = cv2.GaussianBlur(gray, (3, 3), 0)
+	blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rectKernel)
+
+	# computing the gradient of the blackhat image can improve MRZ detection accuracy
+	# compute the gradient magnitude representation of the blackhat image using Sobel operator
+	# and scale the result into the range [0, 255]
+	gradX = cv2.Sobel(blackhat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+	gradX = np.absolute(gradX)
+	(minVal, maxVal) = (np.min(gradX), np.max(gradX))
+	gradX = (255 * ((gradX - minVal) / (maxVal - minVal))).astype("uint8")
+	cv2.imwrite("mrz_gradx.png", gradX)
+
+	# apply a closing operation using the rectangular kernel to close
+	# gaps in between letters -- then apply Otsu's thresholding method
+	gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+	thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+	cv2.imwrite("mrz_otsu.png", thresh)
